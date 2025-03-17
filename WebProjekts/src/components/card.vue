9f94ec9bef48e3776c0e53142ecd9cff38918bad
22DP2RAlek <131154373+22DP2RAlek@users.noreply.github.com>
2025-03-17 10:36:13 +0000

    vue

---
+++ WebProjekts/src/components/card.vue
@@ -0,0 +1,42 @@
+<!-- Card.vue -->
+<template>
+    <div class="card-container">
+      <div class="card">
+        <img src="https://zz.lv/wp-content/uploads/2023/09/Jelgava-scaled.jpg" alt="Jelgava">
+        <div class="card-content">
+          <h3>Jelgava</h3>
+          <p>Jelgava ir skaista pilsēta netālu no Rīgas.</p>
+          <button>Apskatīt</button>
+        </div>
+      </div>
+  
+      <div class="card">
+        <img src="https://static.lsm.lv/media/2024/07/large/1/o4sa.jpg" alt="Liepaja">
+        <div class="card-content">
+          <h3>Liepāja</h3>
+          <p>Liepāja ir trešā lielākā pilsēta Latvijā. Tā ir lielākā Kurzemes pilsēta.</p>
+          <button>Apskatīt</button>
+        </div>
+      </div>
+  
+      <div class="card">
+        <img src="https://www.sundaypost.com/wp-content/uploads/sites/13/2018/05/iStock-527672859.jpg.jpg" alt="Riga">
+        <div class="card-content">
+          <h3>Rīga</h3>
+          <p>Lielākā Latvijas pilsēta un Latvijas galvaspilsēta ir Rīga.</p>
+          <button>Apskatīt</button>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "Card",
+  };
+  </script>
+  
+  <style scoped>
+  @import './card.css'; /* Import external CSS file for this component */
+  </style>
+  
---
+++ WebProjekts/src/components/login.vue
@@ -0,0 +1,35 @@
+<template>
+    <main>
+      <p> Reģistrācija</p>
+      <input 
+        type="text" 
+        v-model="email" 
+        id="email" 
+        name="email" 
+        placeholder="Ievadi savu e-pastu" 
+      />
+      <input 
+        type="password" 
+        v-model="password" 
+        id="password" 
+        name="password" 
+        placeholder="Ievadi savu paroli" 
+      />
+      <button @click="register">Reģistrēties</button>
+    </main>
+  </template>
+  
+  <script>
+  // If you don't need logic right now, you can leave the script section empty
+  export default {
+    // No data or methods for now
+  }
+  </script>
+  
+  <style scoped>
+  main {
+    text-align: center;
+    padding-top: 10%;
+  }
+  </style>
+  
